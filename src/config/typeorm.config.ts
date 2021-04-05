f9b0217f7b15bc0ae29696429388bca95088af30
Matheus Vieira <maath.vieeira@gmail.com>
2021-04-05 15:17:48 -0300

    adjustment

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'sql210.main-hosting.eu',
+    host: '213.190.6.64',
     port: 3306,
-    username: 'u144364835_house_root ',
-    password: 'j&v+VKX6n?',
+    username: 'u144364835_house_root',
+    password: "F3d4glHbmrv",
     database: 'u144364835_houses_got',
     entities: [__dirname + '/../**///**.entity{.ts,.js}'],
-    synchronize: true,
+    synchronize: false,
     ssl: false,
 };
