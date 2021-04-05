76dd464248573dc90b698babdaa8ef75949129be
Matheus Vieira <maath.vieeira@gmail.com>
2021-04-05 15:02:05 -0300

    add config base

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'localhost',
+    host: 'sql210.main-hosting.eu',
     port: 3306,
     username: 'u144364835_house_root ',
     password: 'j&v+VKX6n?',
